3f5a59ae8d3515755f0a200686d776e497ac63e4
mike2477 <mthomas.denver@gmail.com>
2023-04-29 07:06:12 -0600

    added lines for each fuel, responsiveness

--- js/fuel_trends.js
+++ js/fuel_trends.js
@@ -4,15 +4,26 @@ async function drawLineChart() {
   // access data
   const dataset = await d3.json("../data/fuel_trends.json")
 
+  // sorted by 2020 coal generation
+  const sortedData = Object.entries(dataset)
+    .map(([stateName, stateData]) => ({ stateName, coalGeneration: stateData[stateData.length - 1].coal_proportion }))
+    .sort((a, b) => b.coalGeneration - a.coalGeneration)
+
+  console.log(dataset)
+  console.log(sortedData)
+
+
+  const windowWidth = window.innerWidth;
+
   for (const state in dataset) {
 
     const stateData = dataset[state]
     const stateName = state
 
-    // set dimensions
-    const width = 200;
-    const height = 200;
-    const margin = {top: 50, right: 20, bottom: 40, left: 40};
+    // set dimensions based on window width
+    const width = windowWidth < 520 ? 300 : 200;
+    const height = windowWidth < 520 ? 300 : 200;
+    const margin = {top: 50, right: 40, bottom: 40, left: 40};
     
     // create svg
     const svg = d3.select("#chart")
@@ -20,7 +31,7 @@ async function drawLineChart() {
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
     // accessors
     const xAccessor = d => new Date(d.year, 0, 1)  // turn d.year which is integer into a date object to use in d3 time scale later
@@ -42,20 +53,28 @@ async function drawLineChart() {
       .attr("text-anchor", "middle")
       .attr("font-size", "16px")
       .attr("font-weight", "bold")
-      .text(`${stateName} Fuel Trends`);
+      .text(stateName);
 
-    // create line generator
-    const lineGenerator = d3.line()
-      .x(d => xScale(xAccessor(d)))
-      .y(d => yScale(yAccessor(d)));
+      const fuelColors = {
+        coal_proportion: "#DF7A5F",
+        natural_gas_proportion: "#F1CC90",
+        hydroelectric_proportion: "#5E60CE",
+        nuclear_proportion: "#3D405B",
+        wind_proportion: "#80B29B"
+      };
 
-    // draw line
-    svg.append("path")
-      .datum(stateData)
-      .attr("d", lineGenerator)
-      .attr("fill", "none")
-      .attr("stroke", '#DF7A5F')
-      .attr("stroke-width", 3)
+      for (const fuelType of ["coal_proportion", "natural_gas_proportion", "hydroelectric_proportion", "nuclear_proportion", "wind_proportion"]) {
+        const lineGenerator = d3.line()
+          .x(d => xScale(xAccessor(d)))
+          .y(d => yScale(d[fuelType]));
+      
+        svg.append("path")
+          .datum(stateData)
+          .attr("d", lineGenerator)
+          .attr("fill", "none")
+          .attr("stroke", fuelColors[fuelType])
+          .attr("stroke-width", 2);
+      }
 
     // draw axes
     const xAxis = d3.axisBottom(xScale)
